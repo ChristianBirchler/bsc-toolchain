bc031983660fde51d91fd9e0a5a4b5251e482612
Christian Birchler <christian.birchler2@uzh.ch>
2020-03-25 20:33:39 +0100

    extract names; cache projects

--- toolchain.py
+++ toolchain.py
@@ -4,6 +4,7 @@ import sys
 import csv
 import time
 import os
+import re
 
 class bcolors:
     HEADER = '\033[95m'
@@ -15,37 +16,60 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def get_unique_urls(dataset):
+
+def get_unique_urls_names(dataset):
     print("extract project urls ...")
     unique_urls_lst = []
     with open(dataset, "r") as file:
-        csv_reader = csv.reader(file, delimiter=",") 
+        csv_reader = csv.reader(file, delimiter=",")
         for row in csv_reader:
             if row[0] not in unique_urls_lst:
                 print("add: " + row[0])
                 time.sleep(0.1)
-                unique_urls_lst.append(row[0])
-    return unique_urls_lst
+                match = re.match(r".*/(.*$)", row[0])
+                if match:
+                    unique_urls_lst.append((row[0],match.group(1)))
+    return unique_urls_lst[1:]
 
 
-def clone(url):
+def clone(url, proj_dir):
     print(bcolors.OKBLUE + "start cloning " + bcolors.ENDC + bcolors.BOLD + url + bcolors.ENDC)
-
+    os.chdir(proj_dir)
     bash_cmd = "git clone " + url + ".git"
     os.system(bash_cmd)
-
-    print(bcolors.OKGREEN + "clone successful" + bcolors.OKGREEN)
-
-
-def clone_projects(project_urls, proj_dir):
-    print("Start cloning projects to " + proj_dir)
-    os.chdir(proj_dir)
-    for url in project_urls:
-        clone(url)
     os.chdir("../")
+    print(bcolors.OKGREEN + "clone successful" + bcolors.ENDC)
 
-def get_projects(proj_dir):
-    pass
+
+def delete_projects(proj_dir):
+    print("delete projects in " + proj_dir)
+    os.system("rm -rf "+proj_dir+"*")
+    
+
+def get_project_names(project_urls):
+    print("extract project names ...")
+    proj_names = []
+    for url in project_urls:
+        name = re.match(r".*/(.*$)", url).group(1)
+        proj_names.append(name)
+        print("name: " + name)
+    return proj_names
+
+
+def run_metric_gathering_on(proj_dir, proj_name):
+    print("run metric gathering on " + bcolors.UNDERLINE + proj_name + bcolors.ENDC)
+    os.chdir(proj_dir+proj_name)
+
+    
+
+    os.chdir(ROOT)
+    
+
+
+
+
+
+
 
 if __name__ == "__main__":
     print("start toolchain script ...")
@@ -53,29 +77,39 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         raise Exception(f"{bcolors.FAIL}one file (data) argument is needed{bcolors.FAIL}")
 
-    # clone all projects specified in csv file
 
     # iterate over projects
+    #   clone project
     #   iterate over all commits (based on dataset) of the current project
     #       build project
     #       iterate 10 times
     #           run test suite with java agent and mark the specific flaky test
     #           serialize the collected metrics and store into file (one file with 10 entries for each entry in the flaky dataset)
+    #   delete project to save space
 
 
     # PARAMETERS
     DATASET = sys.argv[1]
+    ROOT = os.getcwd()
     PROJ_DIR = "./projects-clones/"
-    RESET_PROJ_DIR = True
+    RESET_PROJ_DIR = False
+    KEEP_ALL_PROJ_IN_LOOP = True
 
     if RESET_PROJ_DIR:
         os.system("rm -rf " + PROJ_DIR)
         os.system("mkdir " + PROJ_DIR)
 
-    project_urls = get_unique_urls(DATASET)
-    clone_projects(project_urls, PROJ_DIR)
+    project_urls_names = get_unique_urls_names(DATASET)
+    #project_names = get_project_names(project_urls)
 
-    projects = get_projects(PROJ_DIR)
+    for i in range(len(project_urls_names)):
+        if RESET_PROJ_DIR:
+            clone(project_urls_names[i][0], PROJ_DIR)
+        
+        run_metric_gathering_on(PROJ_DIR, project_urls_names[i][1])
+
+        if not KEEP_ALL_PROJ_IN_LOOP:
+            delete_projects(PROJ_DIR)
 
 
 
